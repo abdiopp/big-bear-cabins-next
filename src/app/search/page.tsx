5959b674e6203f87e32c918995ec36b4d9c2caa5
abdiopp <ginnieabdullah007@gmail.com>
2026-02-10 01:08:04 +0500

    fix:build

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { SearchPage } from "@/components/SearchPage";
 
 export default function Search() {
-  return <SearchPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchPage />
+    </Suspense>
+  );
 }
